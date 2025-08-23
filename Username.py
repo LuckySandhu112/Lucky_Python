0c963fd951fe97b7a2cbcb5415c887d603e2a5f0
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-08-23 12:22:03 +0530

    Add files via upload

---
+++ Username.py
@@ -0,0 +1,8 @@
+Name1 = input("Enter first persons name: ")
+Name2 = input("Enter second persons name: ")
+Name3 = input("Enter the third persons name: ")
+string = ("@gmail.com")
+
+print(Name1 + string)
+print(Name2 + string)
+print(Name3 + string)
