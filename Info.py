d840cb0ed42991663d02d333ef0d5fde6fe127fa
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-08-23 10:27:02 +0530

    Add files via upload

---
+++ Info.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+roll_no = int(input("Enter the roll no."))
+
+print("Name: ", name)
+print("Age: ", age)
+print("Roll no.: ", roll_no)
