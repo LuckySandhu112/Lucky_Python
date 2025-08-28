9f469a769713b59f607f3ab569979e7bed1867c2
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-08-28 21:51:00 +0530

    Add files via upload

---
+++ Scholarship.py
@@ -0,0 +1,7 @@
+Income = int(input("Enter your father's income: "))
+Marks = int(input("Enter your marks: "))
+
+if(Income <= 500000 and Marks >= 90):
+    print("Yes, you are eligible for scholarship. ")
+else:
+    print("No, you are not eligible for scholarship. ")
