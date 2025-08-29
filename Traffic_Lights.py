308db14f9ca1986bb82ef2f63a728f754cc4a14c
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-08-29 23:08:28 +0530

    Add files via upload

---
+++ Traffic_Lights.py
@@ -0,0 +1,10 @@
+Colour = input("Enter the colour: ")
+
+if(Colour == 'Red'):
+    print("Stop")
+elif(Colour == 'Yellow'):
+    print("Get Ready")
+elif(Colour == 'Green'):
+    print("Go")
+else:
+    print("...Invalid colour...")
