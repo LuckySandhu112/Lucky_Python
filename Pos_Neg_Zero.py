794243c9d90815d8db03b3e029f2bdd5d72f2a5a
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-08-29 23:06:06 +0530

    Add files via upload

---
+++ Pos_Neg_Zero.py
@@ -0,0 +1,10 @@
+Num = int(input("Enter the number: "))
+
+if(Num < 0):
+    print("It is a negative number. ")
+elif(Num > 0):
+    print("It is a positive number. ")
+elif(Num == 0):
+    print("It is zero. ")
+else:
+    print("Invalid....")
