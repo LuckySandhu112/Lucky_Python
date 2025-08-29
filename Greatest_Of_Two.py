11ba6648c6f0db18abe64a1372cdf5851caf91db
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-08-29 23:06:40 +0530

    Add files via upload

---
+++ Greatest_Of_Two.py
@@ -0,0 +1,11 @@
+Num1 = int(input("Enter the first number: "))
+Num2 = int(input("Enter the second number: "))
+
+if(Num1 > Num2):
+    print(Num1, "is greater then", Num2)
+elif(Num1 < Num2):
+    print(Num2, "is greater then", Num1)
+elif(Num1 == Num2):
+    print(Num1, "is equal to", Num2)
+else:
+    print("Invalid....")
