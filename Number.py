f4e1e5431a918cc4125886582b5f04d14326c7e7
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-09-08 23:22:53 +0530

    Add files via upload

---
+++ Number.py
@@ -0,0 +1,8 @@
+Num = int(input("Enter the number: "))
+
+if(Num < 10):
+    print(Num, "less then 10.  ")
+elif(Num >= 10 and Num <= 20):
+    print(Num, "Is between 10 and 20. ")
+elif(Num > 20):
+    print(Num, "is greater then 20. ")
