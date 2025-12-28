c8876cf6e204c91cb05c86b651793f5b83898274
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-12-28 20:13:24 +0530

    Add tuple operations example in Tuple_Operations.py
    Demonstrate tuple operations including length, index, and count.

---
+++ Tuple_Operations.py
@@ -0,0 +1,13 @@
+A = ("Apple","Banana",1,2,1,3,1,1)
+print(len(A))
+print(A.index("Banana"))
+print(A.count(1))
+a1 = (1,2,3,4,5,6)
+list1 = list(a1)
+
+
+
+t1 = (1,2,3,4,5)
+t2 = (7,8,9)
+t3 = t1 + t2
+print(len(t3))
