1582869caa05323cb086b8b397f5a49b8c4d172a
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-12-28 20:14:28 +0530

    Implement list operations and print results

---
+++ List_Operations.py
@@ -0,0 +1,8 @@
+pairs = [(1,2),(3,4),(5,6)]
+result = (sum(x,y)for x, y in pairs)
+print(result)
+s1 = ['A','B','C',1,2,3,4]
+print(s1)
+s2 = ['Hat','his','9','adi','rahul','you','8']
+s1.update(s2)
+print(s1)
