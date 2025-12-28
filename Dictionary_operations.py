325f0d2195395d3143e54b44673b0d16a9af88bb
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-12-28 20:17:17 +0530

    Create Dictionary_operations.py

---
+++ Dictionary_operations.py
@@ -0,0 +1,11 @@
+Employee = dict({"Name": "Amit", "Age": 32, "Salary": 6000, "Company": "TCS"})
+Employee["YEAR"] = 2000
+print(Employee)
+Employee["salary"]=50000
+print(Employee)
+Employee["home"]="India"
+print(Employee)
+Employee.clear()
+print(Employee)
+Employee.copy()
+print(Employee)
